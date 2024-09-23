e9bffdad1df7febc7e261259081ef91feb3309e3
Joanne Kuang <jkuang11@myseneca.ca>
2024-09-23 18:36:06 -0400

    added my lab2a1.py

---
+++ lab2a1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author: Joanne Kuang
+# Date: 09-23-2024
+# Program: lab2a1.py
+
+name = 'Jon'
+age = 20
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+colour = input("Type in a colour and press enter: ")
+
