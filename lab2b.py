229c119368585f369b81746ac3e229303039a598
Joanne Kuang <jkuang11@myseneca.ca>
2024-09-23 18:37:45 -0400

    added my lab2b.py

---
+++ lab2b.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Author: Joanne Kuang
+# Date: 09-23-2024
+# Program: lab2b.py
+
+'''
+Input / Output Requirements
+
+The script should have a Shebang line
+The script should use an object called name
+The script should use an object called age
+The script should prompt the user for "Name: "
+The script should prompt the user for "Age: "
+The script should store the values in the correctly spelled objects (case sensitivity counts)
+The script should print the EXACT OUTPUT as shown (case sensitivity counts
+
+'''
+name = input("Name: ")
+age = input("Age: ")
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
