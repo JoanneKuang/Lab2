a66a53d8d600ff5decf39b4b8c97a3d015d72d72
Joanne Kuang <jkuang11@myseneca.ca>
2024-09-23 18:38:31 -0400

    added my lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Author: Joanne Kuang
+# Student ID: 165994187
+# Date: 2024-09-23
+# Program: lab2e.py
+
+'''
+Input / Output / Processing Requirements
+
+The script should have a Shebang line
+The script should have a comment line which contains the word "Author:" and your full name
+The script should have another comment line which contains the word "Author ID:" and your seneca_id
+The script should have another comment line which contains the word "Date Created:" and the actual date in "yyyy/mm/dd" format
+The script should use an integer object named timer with a value of 10
+The script should have a while loop that repeats until timer equals 0
+The script should print the EXACT OUTPUT as shown
+
+'''
+# this will count down from 10
+count = 10
+
+while count > 0:
+	print(count)
+	count = count - 1
+print('blast off!')
